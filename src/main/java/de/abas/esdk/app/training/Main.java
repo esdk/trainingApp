e0ec7343abbd5bdd5550868fb712e0107a61d7c3
rhaendel <rhaendel@users.noreply.github.com>
2018-10-12 16:13:06 +0200

    ESDK-917 Call license checker in trainingApp

--- src/main/java/de/abas/esdk/app/training/Main.java
+++ src/main/java/de/abas/esdk/app/training/Main.java
@@ -1,6 +1,5 @@
 package de.abas.esdk.app.training;
 
-import de.abas.erp.api.gui.TextBox;
 import de.abas.erp.axi2.EventHandlerRunner;
 import de.abas.erp.axi2.annotation.ButtonEventHandler;
 import de.abas.erp.axi2.annotation.EventHandler;
@@ -8,7 +7,7 @@ import de.abas.erp.axi2.type.ButtonEventType;
 import de.abas.erp.db.DbContext;
 import de.abas.erp.db.infosystem.custom.ow1.TrainingTest;
 import de.abas.erp.jfop.rt.api.annotation.RunFopWith;
-
+import de.abas.esdk.client.api.license.LicenseChecker;
 
 @EventHandler(head = TrainingTest.class, row = TrainingTest.Row.class)
 @RunFopWith(EventHandlerRunner.class)
@@ -16,7 +15,9 @@ public class Main {
 
 	@ButtonEventHandler(field = "start", type = ButtonEventType.AFTER)
 	public void startAfter(DbContext ctx, TrainingTest infosys) {
-		new TextBox(ctx, "Test", "Test message").show();
+		boolean result = LicenseChecker.instance().validate();
+
+		ctx.out().println("License check passed: " + result);
 	}
 
 }
