0c5a29952b06766bec92284dd1e921408fe42d2f
Rainer Schmitz <Rainer.Schmitz@abas.de>
2021-02-02 12:26:22 +0100

    ESDK-2662 Replace deprecated function call.

--- src/integTest/java/de/abas/esdk/app/training/CustomerMainIntegTest.java
+++ src/integTest/java/de/abas/esdk/app/training/CustomerMainIntegTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.fail;
 
 public class CustomerMainIntegTest extends EsdkIntegTest {
