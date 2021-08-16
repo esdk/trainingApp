2c9985514f11721ef199dad8900e251180774704
Rainer Schmitz <Rainer.Schmitz@abas.de>
2021-08-16 13:46:11 +0200

    ESDK-2843 Added snapshot repository to test against esdk gradle plugin snapshots.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,21 @@
 pluginManagement {
+    val esdkSnapshotURL: String by settings
+    val nexusUser: String by settings
+    val nexusPassword: String by settings
+
     repositories {
         maven {
             url = uri("https://artifactory.abas.sh/artifactory/abas.maven-public/")
+        }
+        maven {
+            url = uri(esdkSnapshotURL)
+            credentials {
+                username = nexusUser
+                password = nexusPassword
+            }
         }
         gradlePluginPortal()
     }
 }
 
 rootProject.name = "trainingApp"
-// end
